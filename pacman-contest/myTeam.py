db21ec5e120ecfd730a514258747ec95d475779b
Mingda Ma <mmd_1995@outlook.com>
2018-10-03 22:03:31 +1000

    10.03 10:00pm

--- pacman-contest/myTeam.py
+++ pacman-contest/myTeam.py
@@ -230,7 +230,7 @@ class ReflexCaptureAgent(CaptureAgent):
         #         'disToBoundary': -6.94156916302, 'deadends': -10}
 
         # ---------situaion 2, pacman is currying more than 9, try to go home
-        if features['dots'] >= 9 and features['oldDots'] != 8:
+        if features['dots'] >= 9 and features['oldDots'] != 8 and features['disToGhost'] != 12:
             newWeights = {'score': 1.78261354182, 'DisToNearestFood': -2.91094492098, 'disToGhost': 8.17572535548,
                           'disToCapsule': -1.36111562824, 'dots': -0.877933155097,
                           'disToBoundary': -6.94156916302, 'deadends': -10}
@@ -249,30 +249,25 @@ class ReflexCaptureAgent(CaptureAgent):
             newWeights['disToBoundary'] = -10
 
         # -------situation 4, when the pacman eat capsule and still time remain, try to eat as much as possible
-        # if features['strong'] > 40 and features['dots'] < 8:
-        #     self.offenceMode = 'crazy'
-        #     # print 'mode: carzy'
-        #     newWeights['disToGhost'] = 0
-        #     newWeights['deadends'] = 0
-        #     newWeights['disToBoundary'] = 0
-        #     newWeights['disToCapsule'] = 0
+        if features['strong'] > 40 and features['dots'] < 8:
+            self.offenceMode = 'crazy'
+            # print 'mode: carzy'
+            newWeights['disToGhost'] = 0
+            newWeights['deadends'] = 0
+            newWeights['disToBoundary'] = 0
+            newWeights['disToCapsule'] = 0
 
 
         # --------situation 5, pacman catching by enemy, dis < 3 and foodcurry > 5, try to eat capsule first
         if features['oldDots'] > 3 and features['disToGhost'] < 3:
 
             # print 'mode: attack back'
-            newWeights['disToCapsule'] = weights['disToCapsule']*5
+            newWeights['disToCapsule']*=5
 
-        # # --------situation 6, pacman must step into the enemy place
-        # if features['dots'] < 1:
-        #     print 'mode: extrme'
-        #     newWeights = {'score': 1.78261354182, 'DisToNearestFood': -22.91094492098, 'disToGhostExtrme': 28.17572535548,
-        #                   'disToCapsule': -1.36111562824, 'dots': -0.877933155097,
-        #                   'disToBoundary': -6.94156916302, 'deadends': -10}
 
-        # print features
-        # print newWeights
+
+        print features
+        print newWeights
         return features * newWeights
 
 
@@ -475,14 +470,14 @@ class OffensiveReflexAgent(ReflexCaptureAgent):
             qval = self.evaluate(gameState, action, agentType)
             # qval = self.evl2(gameState, action)
             # if self.offenceMode == 'crazy':
-            # print "action", action
-            # print qval
+            print "action", action
+            print qval
             if qval >= maxQ:
                 maxQ = qval
                 maxQaction = action
 
         # self.updateWeights(gameState, maxQaction)
-        # print "====================================]=================so i choose:", maxQaction
+        print "====================================]=================so i choose:", maxQaction
         return maxQaction
 
     def getReward(self, gameState, action):
